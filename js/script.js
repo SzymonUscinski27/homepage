55b3a1fb8b01dd3a949ad4644286c255bf7b653e
Szymon Uscinski <szymonuscinski@gmail.com>
2021-05-03 00:22:32 +0200

    toggle background function added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-console.log("Cześć, miło mi powitać Cię na moejej stronie. Życzę powodzenia w kursie! :)");
+{
+    console.log("Cześć, miło mi powitać Cię na moejej stronie. Życzę powodzenia w kursie! :)");
 
-let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-let removeEverythingButton = document.querySelector(".js-removeEverythingButton");
-let body = document.querySelector("body");
+    const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+    const removeEverythingButton = document.querySelector(".js-removeEverythingButton");
+    const showDescriptionButton = document.querySelector(".js-showDescriptionButton");
+    const hiddenParagraph = document.querySelector(".js-hiddenParagraph");
+    const body = document.querySelector("body");
 
-let showDescriptionButton = document.querySelector(".js-showDescriptionButton");
-let hiddenParagraph = document.querySelector(".js-hiddenParagraph");
+    const onToggleBackgroundClick = () => {
+        body.classList.toggle("body__secondBackground");
+        changeBackgroundButton.innerText = body.classList.contains("body__secondBackground") ? "Zmień tło na ciemniejsze" : "Zmień tło na jaśniejsze";
+    };
 
+    showDescriptionButton.addEventListener("click", () => {
+        hiddenParagraph.classList.toggle("paragraph__hidden");
+        showDescriptionButton.innerText = hiddenParagraph.classList.contains("paragraph__hidden") ? "Pokaż opis" : "Ukryj opis";
+    });
 
-changeBackgroundButton.addEventListener("click", () => {
-    body.classList.toggle("body__secondBackground");
+    removeEverythingButton.addEventListener("click", () => {
+        body.remove();
+    });
 
-    changeBackgroundButton.innerText = body.classList.contains("body__secondBackground") ? "Zmień tło na ciemniejsze" : "Zmień tło na jaśniejsze";
-});
+    const init = () => {
+        changeBackgroundButton.addEventListener("click", onToggleBackgroundClick);
+    }
 
-showDescriptionButton.addEventListener("click", () => {
-    hiddenParagraph.classList.toggle("paragraph__hidden");
-
-    showDescriptionButton.innerText = hiddenParagraph.classList.contains("paragraph__hidden") ? "Pokaż opis" : "Ukryj opis";
-});
-
-removeEverythingButton.addEventListener("click", () => {
-    body.remove();
-});
+    init();
+}
